f54fb28bef301495c9edd4fefd2bcf110bbfcb89
Ryui-code <ulanbektalasbaev2020@gmail.com>
2025-12-27 15:09:55 +0600

    Changes

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import uvicorn
 from fastapi import FastAPI
-from fstsite.api import users
+from fstsite.api import users, category, product, review
 app = FastAPI(title='fstsite')
 
 app.include_router(users.users_router)
+app.include_router(category.categories_router)
+app.include_router(review.reviews_router)
+app.include_router(product.products_router)
 
 if __name__ == '__main__':
     uvicorn.run('main:app', reload=True)
